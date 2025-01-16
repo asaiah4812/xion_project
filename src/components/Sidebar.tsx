3d95f0ae7a2276e16c4f6666338c8b0830ddeecc
asaiah4812 <hensonasaiah21@gmail.com>
2025-01-16 11:43:17 +0100

    new

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { BookmarkCheck, Home, PanelLeftClose, PanelRightClose, UserRoundCog, UsersRound } from 'lucide-react'
+import { BookmarkCheck, Home, PanelLeftClose, PanelRightClose, UserRoundCog } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
